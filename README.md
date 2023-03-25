75f0ae3ff54dd965ec268e9912493d42e20676bb
livingsneaky-art <ryanllamido@gmail.com>
2023-03-25 14:40:28 +0800

    Partial notes for week1

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Front-End-Web-Development-with-React
+## Ecommerce System<br>
+
+## Features<br>
+For admin<br>
+(pending)<br>
+
+For customer<br>
+(pending)<br>
+
+## Login Information<br>
+Username:<br>
+Password:<br>
+(pending)<br>
+
+## Note:<br> 
+## Debug //<br>
+## Documentation<br>
+## Refer to `documentation.<br>
+
+## Contributors<br>
+LLAMIDO, RYAN B.<br>
+
+## Project Status<br>
+Ongoing(Week-1) - 03/25/2023<br>
+(Week-2) - /2023<br>
+(Week-3) - /2023<br>
+(Week-4) - /2023<br>
---
+++ week_1.js
@@ -0,0 +1,82 @@
+//Introduction to React
+
+//Welcome to Front-End Web Development with React
+/*
+    Course Outline
+        - React (Library and ecosystem)
+        - Reactstrap (Styling the views in the React component)
+        - Redux (structuring the architecture of your react + redux application)
+        - Fetch (for interacting with the server side from our react application)
+
+    Module 1: Introduction to React
+        - Full Stack Web Development: The Big Picture
+        - Setting up your Development Environment: Git and Node
+        - Introduction to React
+        - React Components
+        - Assignment 1
+    
+    Module 2: React Router and Single Page Applications
+        - React Component Types
+        - React Router
+        - Single Page Applications
+        - Assignment 2
+
+    Module 3: React Forms, Flux Architecture and Introduction to Redux
+        - Controlled Forms
+        - Uncontrolled Forms
+        - Introduction to Redux
+        - React Redux Form
+        - Assignment 3
+
+    Module 4: More Redux and Client-Server Communication
+        - Redux Actions
+        - Redux Thunk
+        - Client-Server Communication
+        - Fetch
+        - React Animations
+        - Building and Deployment
+        - Testing
+        - Assignment 4
+*/
+
+//How to Use the Learning Resources
+
+//Welcome to Front-End Web Development with React: Additional Resouces
+
+
+//Full-Stack Web Development: The Big Picture
+//Full-Stack Web Development: The Big Picture: Objectives and Outcomes
+/*
+    - Understand what is meant by full stack in the context of web development
+    - Distinguish between front-end, back-end and full stack web development
+    - Understand the position of this course in the context of this specialization
+*/
+
+//What is Full-Stack Web Development
+/*
+    Front end and Back end
+        - Front end / Client-side
+            - HTML, CSS, and Javascript
+        - Back end / Server-side
+            - Various technologies and approaches
+            - PHP, Java, ASP.NET, Ruby, Python
+
+    Three Tier Architecture
+        - Presentation layer
+        - Business Logic layer
+        - Data Access layer
+*/
+
+//Full Stack Web Development: Additional Resources
+
+
+//Setting up your Development Environment: Git and Node
+//Setting up Your Development Environment: Git and Node: Objectives and Outcomes
+/*
+    - Set up a Git repository and perform basic Git operations
+    - Set up and use online Git repositories
+    - Use Node-based modules to perform basic operations.
+*/
+
+//Setting up Git
+
