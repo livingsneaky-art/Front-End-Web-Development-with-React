f6fe8ec0b53c907f4787a55dd530c5b811a8241a
livingsneaky-art <ryanllamido@gmail.com>
2023-06-14 12:15:41 +0800

    Update week_1.js

--- week_1.js
+++ week_1.js
@@ -388,6 +388,30 @@
         - Updated the view of the DishdetailComponent to display the list of comments about the dish using the Bootstrap unstyled list component
 */
 
+//Ideation
+/*
+    Ideation: Objectives and Outcomes
+        - Clearly express the central idea of your project, and identify the problem being addressed
+        - Delineate a set of features that you expect your website and app should support
+        - Identify other projects that might have similar features and would act as exemplars for your project
+
+    Idetaion Report Template
+        Project Title
+            1. Introduction 
+                - A brief introduction to your website idea. State the goals of the project
+                - The values / benefits (tangible and intangible) this application can bring to a company/organization/end-user
+            2. Expected List of Features
+                - A brief list of features that you expect your website to support
+                - Brief justifications for including these features
+            3. Market Survey
+                - Do a survey of the Web to find about five web sites that might have similar ideas as yours
+                - Briefly compare the features of these applications with your application idea
+            4. References
+                - Give references to any material / website / books etc. relevant to your application idea
+                - Give the links to the websites relevant to your idea, that you listed in the section above.
+*/  
+
+
 
 
 
