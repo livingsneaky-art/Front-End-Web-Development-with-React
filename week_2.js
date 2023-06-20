c3751f811ad25ece040c68a97ee3650473224846
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-06-20 17:34:38 +0800

    Update week_2.js

--- week_2.js
+++ week_2.js
@@ -1,4 +1,19 @@
+//React Router and Single Page Applications
+/*
+    Learning Objectives
+        - Develop a React router based navigation to various view in your React application
+        - Develop a single page application using the React router support
+        - Give examples of various types of React components
+/*
 
+//React Component Types
+/*
+    Objectives and Outcomes
+        - Identify the salient features and uses for the various types of components
+        - Create presentational, container and functional components in your React application
+*/
+
+//Presentational and Container Components
 
 
 
